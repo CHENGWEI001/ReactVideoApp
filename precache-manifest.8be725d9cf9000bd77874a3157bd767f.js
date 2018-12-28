d54e776c965140af233afbb2190d23494fb6fae9
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2018-12-28 23:36:35 +0000

    Updates

---
+++ precache-manifest.8be725d9cf9000bd77874a3157bd767f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa513c13071d2f688e62",
+    "url": "/ReactVideoApp/static/js/runtime~main.fa513c13.js"
+  },
+  {
+    "revision": "ca1d92c7f17c5f921018",
+    "url": "/ReactVideoApp/static/js/main.ca1d92c7.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/ReactVideoApp/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "ca1d92c7f17c5f921018",
+    "url": "/ReactVideoApp/static/css/main.6876bf1e.chunk.css"
+  },
+  {
+    "revision": "a8ea1399d69614b47836d2bd996ced11",
+    "url": "/ReactVideoApp/index.html"
+  }
+];
